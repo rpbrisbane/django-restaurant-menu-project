66d8907812f97ae23d353e0a07d8e7d154e149f0
patrick <patrickdlacey@gmaillcom>
2024-04-15 20:29:29 -0700

    add Item class with models & Add Meal type tuple

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main_dishes", "Main Dishes"),
+    ("desserts", "Desserts")
+)
+
+
+class Item(models.Model):
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(decimal_places=2)
+    meal_type = models.CharField(choices=MEAL_TYPE)

add508d209348c15f2c7bc71f8b9cc0a8ba7d784
patrick <patrickdlacey@gmaillcom>
2024-04-15 21:10:02 -0700

    Create urls.py import include and include urls.py from restaurant_menu, add comments for clarity

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -4,7 +4,10 @@ from .models import Item
 
 
 class MenuList(generic.ListView):
+    # connects model to the view
     queryset = Item.object.order_by("-date_created")
+
+    # template name connects view to html page
     template_name = "index.html"
 
 

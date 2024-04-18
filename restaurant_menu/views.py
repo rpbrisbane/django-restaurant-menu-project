d77dde47721eacc752f7791f0f73332f94942a3b
patrick <patrickdlacey@gmaillcom>
2024-04-17 17:03:05 -0700

    add meals to index.html with jinja code

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 
 class MenuList(generic.ListView):
@@ -9,6 +9,11 @@ class MenuList(generic.ListView):
 
     # template name connects view to html page
     template_name = "index.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
+        return context
 
 
 class MenuItemDetail(generic.DetailView):

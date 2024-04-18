447b726a5defd4608467b122909006542e889782
patrick <patrickdlacey@gmaillcom>
2024-04-17 18:22:34 -0700

    fixed a bug in index.html

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -10,7 +10,9 @@ class MenuList(generic.ListView):
     # template name connects view to html page
     template_name = "index.html"
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    context_object_name = 'item_list'
+
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["meals"] = MEAL_TYPE
         return context
